5fe028dba5d7266798b122d35a9cebec0614be4c
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-02 21:54:17 -0400

    Phase 1 fewpjs js fundamentals arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+var multiply = num1*num2;
+var random = Math.floor(Math.random()*10) +  1;
+let num3 = 36;
+let num4 = 8;
+var mod = num3 % num4;
+var max = Math.max(2,3,4,5,10,20);
